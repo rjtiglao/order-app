b67c916064ae81e2d15bb9586786b0d660f1a4ea
Rj Tiglao <rjtiglao@gmail.com>
2019-11-03 17:19:51 -0800

    added styles and models

--- models/burger.js
+++ models/burger.js
@@ -1,19 +1,23 @@
-const orm = require ('.././config/orm.js');
+var orm = require("../config/orm");
 
-
-
-burger = {
-
-select: function(stupidasscallback){
-//This will go through the mysql library and push theburger types to burgers
-orm.selectAll(function(result) {
-    //we should be using promises here instead 
-    stupidasscallback(result)
-    })
-},
-
-}
-
-
-module.exports = burger;
-    
+var call = {
+    all: function(burger) {
+      orm.all("burgers", function(res) {
+        burger(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.create("burgers", cols, vals, function(res) {
+        burger(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+  // Export the database functions for the controller (catsController.js).
+  module.exports = call;
