4a549ece41a65460857c4d7873cb85a19304bb11
Rj Tiglao <rjtiglao@gmail.com>
2019-12-02 23:21:33 -0800

    made view changes

--- models/burger.js
+++ models/burger.js
@@ -1,18 +1,13 @@
-const orm = require ('.././config/orm.js');
-
-
+const orm = require(".././config/orm.js");
 
 burger = {
-
-select: function(stupidasscallback){
-//This will go through the mysql library and push theburger types to burgers
-orm.selectAll(function(result) {
-    //we should be using promises here instead 
-    stupidasscallback(result)
-    })
-},
-
-}
-
+  select: function(callback) {
+    //This will go through the mysql library and push theburger types to burgers
+    orm.selectAll(function(result) {
+      //we should be using promises here instead
+      callback(result);
+    });
+  }
+};
 
 module.exports = burger;
