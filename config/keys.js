f697d32404af337fb078efb96c325e125fe874ee
Rj Tiglao <rjtiglao@gmail.com>
2019-12-03 00:18:06 -0800

    had to add keys

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+username = "gptesting";
+password = "test12341234";
+database = "burgers_db";
+port = "3306";
+host = "database-1.cm4p7piy3acq.us-west-1.rds.amazonaws.com";
+
+module.exports = { username, password, database, port, host };
